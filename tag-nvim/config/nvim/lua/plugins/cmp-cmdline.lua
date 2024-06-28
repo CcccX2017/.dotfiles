e5c0996d24c22a3c671eb1fdd40bc3cbf104e586
CcccX <nebulapsns@gmail.com>
2024-06-28 22:26:19 +0800

    完善'cmp-cmdline'配置

--- tag-nvim/config/nvim/lua/plugins/cmp-cmdline.lua
+++ tag-nvim/config/nvim/lua/plugins/cmp-cmdline.lua
@@ -13,12 +13,24 @@ return {
     opts = function()
       local cmp = require("cmp")
       cmp.setup.cmdline(":", {
-        -- mapping = cmp.mapping.preset.cmdline(),
+        mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
           { name = "path" },
         }, {
-          { name = "cmdline" },
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
         }),
+      })
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
       })
     end,
   },
