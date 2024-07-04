bda069222cafd71253335f593477fd58d5fa0f77
CcccX <nebulapsns@gmail.com>
2024-07-04 10:42:14 +0800

    添加插件'better-escape'

--- tag-nvim/config/nvim/lua/config/keymaps.lua
+++ tag-nvim/config/nvim/lua/config/keymaps.lua
@@ -8,7 +8,7 @@ local opts = { noremap = true, silent = true }
 -- jk回到normal模式
 map("i", "jk", "<esc>", { desc = "jk回到normal模式" })
 
-map("n", "x", '"_x')
+map({ "n", "v" }, "x", '"_x')
 
 -- leader w 保存
 map("n", "<leader>w", ":w<cr>", opts)
